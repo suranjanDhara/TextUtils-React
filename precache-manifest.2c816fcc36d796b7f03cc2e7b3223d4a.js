e44bd04768359f57a842d8ee711c47b011006060
suranjanDhara <108186929+suranjanDhara@users.noreply.github.com>
2022-12-25 17:47:33 +0530

    Updates

---
+++ precache-manifest.2c816fcc36d796b7f03cc2e7b3223d4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d01fe56bba9dfd3a9f4",
+    "url": "/TextUtils-React/static/css/main.86b7519a.chunk.css"
+  },
+  {
+    "revision": "3d01fe56bba9dfd3a9f4",
+    "url": "/TextUtils-React/static/js/main.3d01fe56.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/TextUtils-React/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "3c016175441de394fef9",
+    "url": "/TextUtils-React/static/js/2.3c016175.chunk.js"
+  },
+  {
+    "revision": "c664369cb110a7f6239b",
+    "url": "/TextUtils-React/static/js/runtime~main.c664369c.js"
+  },
+  {
+    "revision": "dc20591cbc8f6a05a50c68067158bced",
+    "url": "/TextUtils-React/index.html"
+  }
+];
